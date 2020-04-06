4b22bb4c70eb903477d7d4bbdfc92d2d8b8e9bcb
SreeramThatavarthi <56222315+SreeramThatavarthi@users.noreply.github.com>
2020-04-06 17:00:10 +0530

    Updated

--- resources/js/script2.js
+++ resources/js/script2.js
@@ -34,13 +34,6 @@ $(document).ready(function (){
         var nav = $('.js--main-nav');
         var icon = $('.js--nav-icon i');
         
-        nav.slideToggle(200);
-        if (icon.hasClass('ion-navicon-round')) {
-            icon.addClass('ion-close-round');
-            icon.removeClass('ion-navicon-round');
-        } else {
-            icon.addClass('ion-navicon-round');
-            icon.removeClass('ion-close-round');
-        }        
+        nav.slideToggle(200);     
     });
 });
